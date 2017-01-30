e004e7214a6bee2ece96e24cc34ad6d490ed8066
virtys <sergei.blade14@gmail.com>
2017-01-30 17:40:33 +0300

    gessing-game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.mid = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.min + this.max) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
